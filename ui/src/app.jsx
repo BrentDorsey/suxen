229c08ca3a3097a96313a63b7d5999ebe5c3a70d
jeff-knurek <j.knurek@travelaudience.com>
2019-02-05 09:54:10 +0100

    Improve local dev (#3)
    * CLIENT: change the way apollo is initialized
    
    * add back in the binaries as a temp workaround for frontend tweaks

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import style from './app.scss';
 import Search from "./search";
 
@@ -9,9 +10,13 @@ import {
   Switch,
   BrowserRouter,
 } from 'react-router-dom';
+import {HttpLink} from "apollo-boost";
 
 
-const client = new ApolloClient({uri: '/query'});
+const client = new ApolloClient({
+ link: new HttpLink({ uri: "/query" }),
+ cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
